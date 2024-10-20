5a6a26a9d28fa654d27b9465e4f74583bd7a269c
virajpatel14 <113036615+virajpatel14@users.noreply.github.com>
2024-10-20 17:03:35 +0530

    change topic

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
         <>
             <div>
                 <p className="pt-5">Welcome</p>
-                <h1>We are lerning MERN stack</h1>
+                <h1>THIS IS MY KAUSHALAM PROJECT FOR ASSESMENT</h1>
             </div>
         </>
     )
